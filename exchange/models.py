3bddf1c19f39cb15126a106ba2c1d0632ce4318f
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2021-09-10 13:36:12 +0530

    modified models.py

--- exchange/models.py
+++ exchange/models.py
@@ -9,12 +9,16 @@ class User(AbstractUser):
     wallet_address=models.CharField(max_length=20)
 
 
+
 class Balance(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     usdt=models.FloatField(default=0)
     btc=models.FloatField(default=0)
     eth=models.FloatField(default=0)
     ada=models.FloatField(default=0)
+
+    def __str__(self):
+        return f"user: {self.user}"
 
 
 class Transfers(models.Model):
@@ -24,6 +28,8 @@ class Transfers(models.Model):
     amount=models.FloatField()
     time=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"From: {self.sender}, To: {self.receiver}, amount: {self.amount} {self.asset}"
 
 class Requests(models.Model):
     user=models.CharField(max_length=32, default=None)
@@ -33,3 +39,6 @@ class Requests(models.Model):
     status=models.CharField(max_length=10, default='Pending')
     financer=models.CharField(max_length=32, default=None)
     complete_time=models.DateTimeField(default=None)
+
+    def __str__(self):
+        return f"User: {self.user}, amount: {self.amount} {self.asset}"
